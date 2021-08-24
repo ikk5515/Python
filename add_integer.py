e078e88f18a6e925672d713f8961a9c27f649430
Ingi Kim <ikk5515@naver.com>
2021-08-25 02:20:21 +0900

    python

---
+++ add_integer.py
@@ -0,0 +1,10 @@
+sum = 0
+
+while 1:
+    num = int(input())
+    if num >= 0:
+        sum += num
+    else:
+        break
+
+print(sum)
---
+++ ten.py
@@ -0,0 +1,7 @@
+max = 10
+
+while 1:
+    num = int(input())
+    if num > max:
+        print(num, 'is too big!')
+        break
