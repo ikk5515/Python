056bd89452eb05bd0589ea7588ad45a35901fa0a
Ingi Kim <ikk5515@naver.com>
2021-08-28 00:14:47 +0900

    python

---
+++ greatest_value_ex.py
@@ -0,0 +1,4 @@
+def myaverage(a,b):
+    return(a + b) /2
+
+print(myaverage(3,4))
---
+++ max_min_hamsu_ex.py
@@ -0,0 +1,9 @@
+def get_max_min(data_list):
+    max_value = max(data_list)
+    min_value = min(data_list)
+
+    return(max_value, min_value)
+
+max_value, min_value = get_max_min([1,2,3,4,5])
+print(max_value)
+print(min_value)    
