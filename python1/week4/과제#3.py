c34fdc23bf891c83f626d7aa66f7e74116797246
Ingi Kim <ikk5515@naver.com>
2021-09-28 03:30:28 +0900

    python

--- python1/week4/과제#3.py
+++ python1/week4/과제#3.py
@@ -1,4 +1,5 @@
-select  = int(input("1: 사각, 2: 삼각, 3: 원 "))
+select = int(input("1: 사각, 2: 삼각, 3: 원 "))
+total = 0
 
 if select == 1:
     width = float(input("가로 입력: "))
@@ -13,4 +14,5 @@ elif select == 3:
     total = 3.14 * (r**2)
 else:
     print("잘못된 입력입니다.")
+
 print(f"도형의 넓이 = {total:0.2f}")
